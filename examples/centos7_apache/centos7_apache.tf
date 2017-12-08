4e2ebb28b6a11c957308554b8e11c5aa6bad23e6
Kent Gruber <kgruber1@emich.edu>
2017-12-08 10:37:01 -0500

    Add remote-exec provisioner to the centos 7 apache box.

--- examples/centos7_apache/centos7_apache.tf
+++ examples/centos7_apache/centos7_apache.tf
@@ -65,4 +65,24 @@ resource "aws_instance" "ec2_instance" {
   tags {
     Name = "skynetTerraformExampleYo"
   }
+
+	provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "centos"
+      private_key = "${file("~/.ssh/id_rsa")}"
+    }
+
+    inline = [
+      "echo 'apache' > /etc/hostname",
+      "sudo yum install vim httpd -y",
+      "sudo cp /etc/httpd/conf/httpd.conf ~/httpd.conf.bak",
+      "sudo bash -c \"curl https://raw.githubusercontent.com/picatz/skynet/master/examples/centos7_apache/httpd.conf > /etc/httpd/conf/httpd.conf\"",
+      "sudo bash -c \"echo 'Made with ♥ by picat' > /var/www/html/index.html\"",
+      "sudo systemctl enable httpd",
+      "sudo systemctl start httpd",
+      "curl localhost"
+    ]
+  }
+
 }
